f8c7d4dd74f6f00c3597b3c3d9cd9f7cc3c27266
krasov <me@frostfree.ru>
2025-09-18 18:18:58 +0300

    finish homework

--- roles/suricata/files/service.d
+++ roles/suricata/files/service.d
@@ -1,13 +1,3 @@
-[Unit]
-Description=Suricata Intrusion Detection Service
-After=network.target
-
 [Service]
-PIDFile=/run/suricata.pid
-Type=forking
-Restart=on-failure
 ExecStart=
 ExecStart=/usr/bin/suricata -c /etc/suricata/suricata.yaml -q 1 --pidfile /run/suricata.pid
-
-[Install]
-WantedBy=multi-user.target
